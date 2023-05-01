8aa6114651ffcc9a98900b21a0a02c398c795beb
ThatEpicBanana <thatepicbanana132@gmail.com>
2023-04-30 21:03:50 -0400

    broken first idea

--- handler/src/config.rs
+++ handler/src/config.rs
@@ -1,6 +1,11 @@
-use std::{path::Path, fs, io, env, process::Command};
+use std::{path::{Path, PathBuf}, fs, io, env, process::Command, fmt::Display};
+
+use ron::ser::PrettyConfig;
+use serde::{Serialize, Deserialize};
 
 use log::*;
+
+// TODO: accept null and {wineprefix}/... for mappings 
 
 // both of these are dirty, but they work
 fn get_home_dir() -> String {
@@ -13,31 +18,50 @@ fn get_username() -> String {
 }
 
 /// Defines a simple replacement mapping
-#[derive(Clone)]
-pub struct Mapping {
-    pub from: String,
-    pub to: String,
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Mapping<V> {
+    pub from: V,
+    pub to: V,
 }
 
-impl Mapping {
+impl Mapping<Unresolved> {
+    pub fn resolve(self, config: &ConfigRes<Unresolved>) -> Mapping<Resolved> {
+        Mapping {
+            from: self.from.resolve(config),
+            to: self.to.resolve(config),
+        }
+    }
+}
+
+impl Mapping<Resolved> {
     pub fn map(&self, string: &str) -> String {
-        string.replace(&self.from, &self.to)
+        string.replace(self.from.get(), self.to.get())
     }
 }
 
 /// Info for running commands on MusicBee
-#[derive(Clone)]
-pub struct Commands {
-    pub wine_command: String,
-    pub wine_prefix: String,
-    pub musicbee_location: String,
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Commands<V> {
+    pub wine_command: V,
+    pub wine_prefix: V,
+    pub musicbee_location: V,
 }
 
-impl Commands {
+impl Commands<Unresolved> {
+    pub fn resolve(self, config: &ConfigRes<Unresolved>) -> Commands<Resolved> {
+        Commands {
+            wine_command: self.wine_command.resolve(config),
+            wine_prefix: self.wine_prefix.resolve(config),
+            musicbee_location: self.musicbee_location.resolve(config),
+        }
+    }
+}
+
+impl Commands<Resolved> {
     pub fn run_command(&self, command: &str) {
-        let mut cmd = Command::new(&self.wine_command);
-         cmd.env("WINEPREFIX", &self.wine_prefix)
-            .arg(&self.musicbee_location)
+        let mut cmd = Command::new(self.wine_command.get());
+         cmd.env("WINEPREFIX", self.wine_prefix.get())
+            .arg(self.musicbee_location.get())
             .arg(command);
 
         debug!("Running command: \n    WINEPREFIX={} wine {} {}", self.wine_prefix, self.musicbee_location, command);
@@ -51,12 +75,20 @@ impl Commands {
 }
 
 /// Info for communication between the handler and the plugin
-#[derive(Clone)]
-pub struct Communication {
-    pub directory: String,
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Communication<V> {
+    pub directory: V,
 }
 
-impl Communication {
+impl Communication<Unresolved> {
+    pub fn resolve(self, config: &ConfigRes<Unresolved>) -> Communication<Resolved> {
+        Communication {
+            directory: self.directory.resolve(config),
+        }
+    }
+}
+
+impl Communication<Resolved> {
     pub fn get_comm_path(&self, name: &str) -> String {
         format!("{}/{name}", self.directory)
     }
@@ -66,16 +98,76 @@ impl Communication {
     }
 }
 
-#[derive(Clone)]
-pub struct Config {
-    pub commands: Commands,
-    pub communication: Communication,
-    pub music_file_mapper: Mapping,
-    pub temporary_file_mapper: Mapping,
+#[derive(Clone, Serialize, Deserialize, Debug)]
+#[serde(transparent)]
+struct Unresolved {
+    template: String
+}
+
+impl Unresolved {
+    pub fn resolve(self, config: &ConfigRes<Unresolved>) -> Resolved {
+        let Unresolved { template } = self;
+        let resolved = if template.contains("{") {
+            template.replace("{wine_prefix}", &config.commands.wine_prefix.resolve(config).get())
+        } else {
+            template.clone()
+        };
+
+        Resolved { template, resolved }
+    }
+}
+
+impl From<&str> for Unresolved {
+    fn from(value: &str) -> Self {
+        Self { template: value.to_string() }
+    }
+}
+
+#[derive(Clone, Serialize, Debug)]
+#[serde(transparent)]
+struct Resolved {
+    template: String,
+    #[serde(skip)]
+    resolved: String,
+}
+
+impl Resolved {
+    pub fn get(&self) -> &str {
+        &self.resolved
+    }
+}
+
+impl Display for Resolved {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.resolved)
+    }
+}
+
+pub type Config = ConfigRes<Resolved>;
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct ConfigRes<V> {
+    pub commands: Commands<V>,
+    pub communication: Communication<V>,
+    pub music_file_mapper: Mapping<V>,
+    pub temporary_file_mapper: Mapping<V>,
     pub detach_on_stop: bool,
 }
 
-impl Config {
+impl ConfigRes<Unresolved> {
+    pub fn resolve(self) -> ConfigRes<Resolved> {
+        let cloned = self.clone();
+        ConfigRes {
+            commands: self.commands.resolve(&cloned),
+            communication: self.communication.resolve(&cloned),
+            music_file_mapper: self.music_file_mapper.resolve(&cloned),
+            temporary_file_mapper: self.temporary_file_mapper.resolve(&cloned),
+            detach_on_stop: self.detach_on_stop
+        }
+    }
+}
+
+impl ConfigRes<Resolved> {
     pub fn map_filename(&self, name: &str) -> String {
         // we don't like \ here
         let name = name.replace('\\', "/");
@@ -101,7 +193,7 @@ impl Config {
     }
 }
 
-impl Default for Config {
+impl Default for ConfigRes<Unresolved> {
     fn default() -> Self {
         let home_dir = get_home_dir();
         let username = get_username();
@@ -113,12 +205,12 @@ impl Default for Config {
             communication: Communication::default(),
             // NOTE: this might be unreasonable, but i'm not sure
             music_file_mapper: Mapping {
-                from: format!("C:/Users/{username}/Music"),
-                to: format!("{home_dir}/Music"),
+                from: "C:/Users/{username}/Music".into(),
+                to: "{home_dir}/Music".into(),
             },
             temporary_file_mapper: Mapping {
-                from: "C:/".to_string(),
-                to: format!("{wine_prefix}/drive_c/")
+                from: "C:/".into(),
+                to: "{wine_prefix}/drive_c/".into(),
             },
             commands,
             detach_on_stop: true,
@@ -126,29 +218,56 @@ impl Default for Config {
     }
 }
 
-impl Default for Commands {
+impl Default for Commands<Unresolved> {
     fn default() -> Self {
         let home_dir = get_home_dir();
 
-        // TODO: this is an unreasonable default
-        let wine_prefix = format!("{home_dir}/Documents/executables/musicbee/.wine");
-
         Self {
-            wine_command: "wine".to_string(),
-            musicbee_location: r#"C:/Program Files/MusicBee/MusicBee.exe"#.to_string(),
-            wine_prefix,
+            wine_command: "wine".into(),
+            musicbee_location: r#"C:/Program Files/MusicBee/MusicBee.exe"#.into(),
+            // TODO: this is an unreasonable default
+            wine_prefix: "{home_dir}/Documents/executables/musicbee/.wine".into(),
         }
     }
 }
 
-impl Default for Communication {
+impl Default for Communication<Unresolved> {
     fn default() -> Self {
         Self {
-            directory: "/tmp/musicbee-mediakeys".to_string(),
+            directory: "/tmp/musicbee-mediakeys".into(),
         }
     }
+}
+
+fn config_folder() -> PathBuf {
+    dirs::config_dir().unwrap()
+        .join("musicbee-mediakeys")
+}
+
+fn config_path() -> PathBuf {
+    config_folder().join("config.ron")
 }
 
 pub fn get_config() -> Config {
-    Config::default()
+    let path = config_path();
+
+    println!("{}", config_path().display());
+
+    if path.exists() {
+        if let Ok(config) = 
+            ron::from_str::<ConfigRes<Unresolved>>(
+                &fs::read_to_string(&path)
+                    .unwrap()
+            )
+        { return config.resolve(); }
+    } 
+
+    let config = ConfigRes::<Unresolved>::default().resolve();
+
+    fs::create_dir_all(config_folder()).unwrap();
+    fs::write(path,
+        ron::ser::to_string_pretty(&config, PrettyConfig::new()).unwrap()
+    ).unwrap();
+
+    config
 }
